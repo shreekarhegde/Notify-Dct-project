a37e087be8135734c4f035fa29429e8eab66b5bf
Shreekar Hegde <shreekarhegde9@gmail.com>
2018-12-10 00:18:17 +0530

    updating

--- app/controllers/posts-controller.js
+++ app/controllers/posts-controller.js
@@ -23,13 +23,12 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     let id = req.params.id;
     let body = req.body;
-    console.log(body.comments, "comments");
-    console.log(body, "applause");
+    
         Post.findOneAndUpdate({ _id: id}, {$set: body.applause}).then((post) => {
             console.log(post);
             res.send(post);    
         }).catch((err) => {
-            res.send(err);
+            res.send(err)
         })
     
     if(body.comments !==`` && body.comments !== null){
