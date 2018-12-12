6682dcc5815913e07a39fa349489fb2fc4b334e2
Shreekar Hegde <shreekarhegde9@gmail.com>
2018-12-12 16:48:40 +0530

    added socket.io

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('./config/db');
+var socket = require('socket.io');
 const port = 3001;
 
 const { routes } = require('./config/routes');
+
 
 app.use(express.json());
 app.use(cors());
@@ -15,3 +17,16 @@ app.listen(port, () => {
     console.log('listening on port', port);
 });
 
+server = app.listen(8080, function(){
+    console.log('server is running on port 8080')
+});
+
+io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('SEND_MESSAGE', function(data){
+        io.emit('RECEIVE_MESSAGE', data);
+    })
+});
