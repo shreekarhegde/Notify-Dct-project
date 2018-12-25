f2b77577e38c152b216678cc20b29d80422265e4
Shreekar Hegde <shreekarhegde9@gmail.com>
2018-12-25 18:22:23 +0530

    Implemented Node Mailer

--- app/models/activity.js
+++ app/models/activity.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 delete mongoose.connection.models['Activity'];
-const { Department }  = require('./department');
+const { Department } = require('./department');
 const { Employee } = require('./employee');
+const nodemailer = require('nodemailer');
 
 const activitySchema = new Schema({
     activityName: {
@@ -27,31 +28,68 @@ const activitySchema = new Schema({
         minlength: 3
     },
     guests: [{
-            type: String
+        type: String
     }],
     schedule: {
         time: {
             type: String,
             required: true
-            },
+        },
         date: {
             type: Date,
             required: true
-            }
         }
+    }
 });
 
-
-activitySchema.post('save', function(next){
+activitySchema.post('save', function (next) {
     let activityId = this._id;
     let departmentId = this.departments;
-    console.log(departmentId);
-    let participants = this.participants;
-    Employee.updateMany({_id: {$in: participants}}, {$push: {activities: activityId}}).then((participants) => {        
-        console.log(participants); 
-    });
-    Department.updateMany({_id: departmentId}, {$push: {activities: activityId}}).then((department) => {        
-        console.log(department); 
+    // console.log(departmentId);
+    let newParticipants = this.participants;
+    Employee.updateMany({ _id: { $in: newParticipants } }, { $push: { activities: activityId } }).then((participants) => {
+        console.log(participants);
+        Employee.find({ _id: { $in: newParticipants } }).then((employees) => {
+            let emails = employees.map(employee => {
+                return employee.bio.email;
+            })
+
+            // Generate test SMTP service account from ethereal.email
+            // Only needed if you don't have a real mail account for testing
+                // create reusable transporter object using the default SMTP transport
+                let transporter = nodemailer.createTransport({
+                   service: "Gmail",
+                    auth: {
+                        user: process.env.MY_EMAIL, // generated ethereal user
+                        pass: process.env.GMAIL_PASSWORD // generated ethereal password
+                    }
+                });
+
+                // setup email data with unicode symbols
+                let mailOptions = {
+                    from: '"shreekar hegde" <shreekarhegde9@gmail.com>', // sender address
+                    to: emails.join(), // list of receivers
+                    subject: 'inviting to the event', // Subject line
+                    text: `you have been invited to the event ${this.activityName}`, // plain text body
+                };
+
+                // send mail with defined transport object
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log('Message sent: %s', info.messageId);
+                    // Preview only available when sending through an Ethereal account
+                    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+                    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+                    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+                });
+            });
+        })
+
+    Department.updateMany({ _id: departmentId }, { $push: { activities: activityId } }).then((department) => {
+        console.log(department);
     }).catch((err) => {
         console.log(err);
     })
